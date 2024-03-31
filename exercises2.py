caf51ab0f2e3979ca6f2cc7381cbbbc3c6c340dc
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-03-31 16:42:03 +0500

    Add files via upload

--- exercises2.py
+++ exercises2.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 
@@ -17,8 +18,9 @@ red=(255,0,0)
 blue=(0,0,255)
 white=(255,255,255)
 black=(0,0,0)
-
-
+all_coloures=[red,blue,white,black]
+cnt_coloures=0
+Size=random.randint(0,10)
 left_button_pressed=False
 
 while going==True:
@@ -26,6 +28,19 @@ while going==True:
         if event.type==pygame.QUIT:
             going=0
             pygame.quit()
+
+    if event.type==pygame.KEYDOWN:
+        if event.key==pygame.K_RIGHT:
+            if cnt_coloures==3:
+                cnt_coloures=0
+            else:
+                cnt_coloures+=1
+
+        if event.key==pygame.K_LEFT:
+            if cnt_coloures==0:
+                cnt_coloures=3
+            else:
+                cnt_coloures-=1
 
     if event.type==pygame.MOUSEBUTTONDOWN and event.button==1:
         print("hhh")
@@ -41,8 +56,8 @@ while going==True:
             #pygame.draw.rect(screen,(255,0,0),(event.pos[0],event.pos[1],5,5))
             current_x=event.pos[0]
             current_y=event.pos[1]
-            pygame.draw.rect(screen,red,(prev_x,prev_y,current_x-prev_x,current_y-prev_y))
-
+            pygame.draw.rect(screen,all_coloures[cnt_coloures],(prev_x,prev_y,current_x-prev_x,current_y-prev_y))
+            
 
     if event.type==pygame.MOUSEBUTTONUP and event.button==1:
         left_button_pressed=False
