06372d052f21d7a28ceec86a6121fd02ef195488
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-04-01 02:20:36 +0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,101 +1,155 @@
-import pygame 
-import random,time
+import pygame
+import random
 
 pygame.init()
 
-going=True
-FPS=pygame.time.Clock()
-FPS.tick(60)
-length=400
-width=600
-screen=pygame.display.set_mode((length,width))
-screen.fill((255,255,255))
+W, H = 1200, 800
+FPS = 60
 
-#image_player_car
-image=pygame.image.load(r"C:\Users\acer-\OneDrive\Рабочий стол\Lab8\car.png")
-rect=image.get_rect()
-rect.center = (160, 520)
+screen = pygame.display.set_mode((W, H), pygame.RESIZABLE)
+pygame.display.set_caption("Arkanoid")
+clock = pygame.time.Clock()
+done = False
+bg = (0, 0, 0)
 
-#image_enemy_car
-image2=pygame.image.load("car2.png")
-rect2=image2.get_rect()
-rect2.center=(random.randint(40,370),0)
+# paddle
+paddleW = 150
+paddleH = 25
+paddleSpeed = 20
+paddle = pygame.Rect(W // 2 - paddleW // 2, H - paddleH - 30, paddleW, paddleH)
 
-#image_coin
-image3=pygame.image.load("coin.png")
-image3=pygame.transform.scale(image3,(50,50))
-rect3=image3.get_rect()
-rect3.center=(random.randint(150,370),0)
+# Ball
+ballRadius = 20
+ballSpeed = 5
+ball_rect = int(ballRadius * 2 ** 0.5)
+ball = pygame.Rect(random.randrange(ball_rect, W - ball_rect), H // 2, ball_rect, ball_rect)
+dx, dy = 1, -1
 
-#speed
-speed=5
+# Timer
+
+timer = pygame.time.get_ticks()
+timer_fonts = pygame.font.SysFont('comicsansms', 40)
+timer_text = timer_fonts.render(f'Time: {timer}', True, (0, 0, 0))
+timer_rect = timer_text.get_rect()
+timer_rect.center = (940, 20)
+
+# Game score
+game_score = 0
+game_score_fonts = pygame.font.SysFont('comicsansms', 40)
+game_score_text = game_score_fonts.render(f'Your game score is: {game_score}', True, (0, 0, 0))
+game_score_rect = game_score_text.get_rect()
+game_score_rect.center = (210, 20)
+
+# Catching sound
+collision_sound = pygame.mixer.Sound('catch.mp3')
+
+
+def detect_collision(dx, dy, ball, rect):
+    if dx > 0:
+        delta_x = ball.right - rect.left
+    else:
+        delta_x = rect.right - ball.left
+    if dy > 0:
+        delta_y = ball.bottom - rect.top
+    else:
+        delta_y = rect.bottom - ball.top
+
+    if abs(delta_x - delta_y) < 10:
+        dx, dy = -dx, -dy
+    if delta_x > delta_y:
+        dy = -dy
+    elif delta_y > delta_x:
+        dx = -dx
+    return dx, dy
+
+
+# block settings
+block_list = [pygame.Rect(10 + 120 * i, 50 + 70 * j,
+                          100, 50) for i in range(10) for j in range(4)]
+color_list = [(random.randrange(0, 255),
+               random.randrange(0, 255), random.randrange(0, 255))
+              for i in range(10) for j in range(4)]
+print(block_list)
+# Game over Screen
+losefont = pygame.font.SysFont('comicsansms', 40)
+losetext = losefont.render('Game Over', True, (255, 255, 255))
+losetextRect = losetext.get_rect()
+losetextRect.center = (W // 2, H // 2)
+
+# Win Screen
+winfont = pygame.font.SysFont('comicsansms', 40)
+wintext = losefont.render('You win yay', True, (0, 0, 0))
+wintextRect = wintext.get_rect()
+wintextRect.center = (W // 2, H // 2)
 INC_SPEED = pygame.USEREVENT + 1
 pygame.time.set_timer(INC_SPEED, 1000)
 
-#font
-game_over_font = pygame.font.SysFont('coin.png', 60)
-game_over_cnt=pygame.font.SysFont("coin.png",60)
+while not done:
 
-
-
-
-def move_enemy_car():
-     rect2.move_ip(0,speed)
-     if (rect2.top > 600):
-      rect2.top = 0
-      rect2.center = (random.randint(30, 370), 0)
-
-def move_player_car():
-    pressed_keys=pygame.key.get_pressed()
-    if rect.left > 0:
-          if pressed_keys[pygame.K_LEFT]:
-              rect.move_ip(-5,0)
-    if rect.right <370 :       
-          if pressed_keys[pygame.K_RIGHT]:
-               rect.move_ip(5,0)
-def move_coin():
-     rect3.move_ip(0,10)
-     if (rect3.top>600):
-      rect3.top=0
-      #rect3.center=(random.randint(30,370),0)
-
-              
-   
-
-cnt=0
-while going==True:
     for event in pygame.event.get():
         if event.type == INC_SPEED:
-              speed += 2
-        if event.type==pygame.QUIT:
-            going=False
+            ballSpeed += 0.05
+            if paddle.width > 75:
+                paddle.width -= 1
+        if event.type == pygame.QUIT:
+            done = True
 
-    if rect.colliderect(rect2):
-            screen.fill((255,0,0))
-            n=game_over_font.render("Game Over", True, (0,0,0))
-            c=game_over_cnt.render("Points: "+str(cnt),False,(0,0,0))
-            screen.blit(n, (80,250))
-            screen.blit(c,(200,60))
-            pygame.display.update()
-            time.sleep(4)
-            pygame.quit()
+    screen.fill(bg)
 
+    [pygame.draw.rect(screen, color_list[color], block)
+     for color, block in enumerate(block_list)]  # drawing blocks
+    pygame.draw.rect(screen, pygame.Color(255, 255, 255), paddle)
+    pygame.draw.circle(screen, pygame.Color(255, 0, 0), ball.center, ballRadius)
 
-    move_player_car()
-    move_enemy_car()
-    move_coin()
-    if rect.colliderect(rect3):
-         rect3.top=0
-         screen.fill((255,0,0))
-         pygame.display.update()
-         cnt+=1
+    # Ball movement
+    ball.x += ballSpeed * dx
+    ball.y += ballSpeed * dy
 
-    screen.fill((255,255,255))
-    screen.blit(image,rect)
-    screen.blit(image2,rect2)
-    screen.blit(image3,rect3)
-    #screen.blit(image2.rect2)
-    pygame.display.update()
-    FPS.tick(60)
+    # Collision left
+    if ball.centerx < ballRadius or ball.centerx > W - ballRadius:
+        dx = -dx
+    # Collision top
+    if ball.centery < ballRadius + 50:
+        dy = -dy
+    # Collision with paddle
+    if ball.colliderect(paddle) and dy > 0:
+        dx, dy = detect_collision(dx, dy, ball, paddle)
 
+    # Collision blocks
+    hitIndex = ball.collidelist(block_list)
 
+    if hitIndex != -1:
+        hitRect = block_list.pop(hitIndex)
+        hitColor = color_list.pop(hitIndex)
+        dx, dy = detect_collision(dx, dy, ball, hitRect)
+        game_score += 1
+        collision_sound.play()
+
+    # Timer
+    seconds = ((pygame.time.get_ticks() - timer) % 60000) / 1000
+    timer_text = timer_fonts.render(f'Time: {seconds}', True, (255, 255, 255))
+    screen.blit(timer_text, timer_rect)
+
+    # Game score
+    game_score_text = game_score_fonts.render(f'Your game score is: {game_score}', True, (255, 255, 255))
+    screen.blit(game_score_text, game_score_rect)
+
+    # Win/lose screens
+    if ball.bottom > H:
+        screen.fill((0, 0, 0))
+        screen.blit(losetext, losetextRect)
+    elif not len(block_list):
+        screen.fill((255, 255, 255))
+        screen.blit(wintext, wintextRect)
+
+    # Paddle Control
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT] and paddle.left > 0:
+        paddle.left -= paddleSpeed
+    if key[pygame.K_RIGHT] and paddle.right < W:
+        paddle.right += paddleSpeed
+
+    pygame.display.flip()
+    clock.tick(FPS)
+pygame.quit()
+            
